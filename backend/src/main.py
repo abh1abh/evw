20e03978b2a0cd675a2c238c1fed3161911a3f40
abh1abh <alexhoyskel@yahoo.no>
2025-08-19 13:12:32 +0200

    update

--- backend/src/main.py
+++ backend/src/main.py
@@ -2,25 +2,21 @@ from __future__ import annotations
 import argparse
 import json
 from pathlib import Path
-from LongCSVReader import LongCSVReader
-from FCFFBuilder import FCFFBuilder
 import yfinance as yf
 from roe import Roe
 from roa import Roa
 from margins import Margin
 
 def main():
-    parser = argparse.ArgumentParser(
-        description=( "EVW MVP: read long-format CSV, compute FCFF, run DCF, write outputs."
-    ))
+    parser = argparse.ArgumentParser()
     parser.add_argument("-t", "--ticker", required=True, help="Path to CSV: Year,Metric,Value")
     # parser.add_argument("-o", "--output_path", required=True, help="Output path")
-    parser.add_argument("--wacc", type=float, default=0.08)
-    parser.add_argument("--ltg", type=float, default=0.025)
-    parser.add_argument("--midyear", action="store_true", default=True)
-    parser.add_argument("--no-midyear", dest="midyear", action="store_false")
-    parser.add_argument("--horizon", type=int, default=5)
-    parser.add_argument("--net-debt", type=float, default=None)
+    # parser.add_argument("--wacc", type=float, default=0.08)
+    # parser.add_argument("--ltg", type=float, default=0.025)
+    # parser.add_argument("--midyear", action="store_true", default=True)
+    # parser.add_argument("--no-midyear", dest="midyear", action="store_false")
+    # parser.add_argument("--horizon", type=int, default=5)
+    # parser.add_argument("--net-debt", type=float, default=None)
     args = parser.parse_args()
     
     ticker = yf.Ticker(args.ticker)
